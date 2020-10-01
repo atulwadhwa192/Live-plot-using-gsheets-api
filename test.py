a1277c60a368025ccf10f73c9616ebb7b0848634
atulwadhwa192 <59009165+atulwadhwa192@users.noreply.github.com>
2020-10-01 23:37:45 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 13:06:14 2020
+
+@author: dell
+"""
+
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
+# a='https://www.googleapis.com/auth/spreadsheets.readonly'
+# b='https://www.googleapis.com/auth/spreadsheets'
+# c='https://www.googleapis.com/auth/drive.readonly'
+# d='https://www.googleapis.com/auth/drive.file'
+# e='https://www.googleapis.com/auth/drive'
+# scope=[a,b,c,d,e]
+# credit=ServiceAccountCredentials.from_json_keyfile_name('E:\ds_jyp\cred.json',scope)
+# client=gspread.authorize(credit)
+# sheet=client.open('document').sheet1
+# sheet=client.open('document').sheet1
+# print(sheet.col_values(1))
